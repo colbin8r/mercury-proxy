b377cf1bade2af9081dfd1bbf40d8d5fcd8416c0
Colby Rogness <colbyrogness@gmail.com>
2018-03-01 04:35:40 -0500

    feat(app.js): Add CORS support
    Add support for making Cross-Origin Resource Sharing (CORS) requests.

--- src/app.js
+++ src/app.js
@@ -18,6 +18,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Enable CORS
+// https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routes
 app.use('/', routes);
 
